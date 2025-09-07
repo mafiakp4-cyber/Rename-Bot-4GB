0b8e326036a81b9a8bf6cb4c9ef834af5f1d3906
mafiakp4-cyber <mafiakp4@gmail.com>
2025-09-07 09:39:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,39 @@
+from threading import Thread
+from flask import Flask
+import os
+
 from pyrogram import Client, idle
 from plugins.cb_data import app as Client2
 from config import *
 import pyromod
-import pyrogram.utils
 
-pyrogram.utils.MIN_CHAT_ID = -1002051194704
-pyrogram.utils.MIN_CHANNEL_ID = -1002051194704
+# Flask dummy server
+server = Flask(__name__)
 
+@server.route('/')
+def home():
+    return "✅ Telegram Rename Bot is running on Render!"
 
+def run_flask():
+    port = int(os.environ.get("PORT", 5000))
+    server.run(host="0.0.0.0", port=port)
 
-bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
+def run_bot():
+    bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
 
+    if STRING_SESSION:
+        apps = [Client2, bot]
+        for app in apps:
+            app.start()
+        idle()
+        for app in apps:
+            app.stop()
+    else:
+        bot.run()
 
-
-
-if STRING_SESSION:
-    apps = [Client2,bot]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-    
-else:
-    bot.run()
+if __name__ == "__main__":
+    Thread(target=run_flask).start()
+    Thread(target=run_bot).start()
 
 
 
