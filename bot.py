826d1fc441af64782708251661829dfdccd5f6d7
mafiakp4-cyber <mafiakp4@gmail.com>
2025-09-07 09:45:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,53 @@
-from threading import Thread
-from flask import Flask
-import os
-
+import asyncio
 from pyrogram import Client, idle
 from plugins.cb_data import app as Client2
 from config import *
 import pyromod
+import pyrogram.utils
+from flask import Flask
+import threading
+import os
 
-# Flask dummy server
-server = Flask(__name__)
+# Flask app (Render ko port bind karne ke liye)
+app_web = Flask(__name__)
 
-@server.route('/')
+@app_web.route('/')
 def home():
-    return "✅ Telegram Rename Bot is running on Render!"
+    return "Bot is running fine on Render!"
 
-def run_flask():
+def run_web():
     port = int(os.environ.get("PORT", 5000))
-    server.run(host="0.0.0.0", port=port)
+    app_web.run(host="0.0.0.0", port=port)
 
-def run_bot():
-    bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
+# Start Flask in background
+threading.Thread(target=run_web).start()
 
+pyrogram.utils.MIN_CHAT_ID = -1002051194704
+pyrogram.utils.MIN_CHANNEL_ID = -1002051194704
+
+bot = Client(
+    "Renamer",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root="plugins")
+)
+
+async def main():
     if STRING_SESSION:
         apps = [Client2, bot]
         for app in apps:
-            app.start()
-        idle()
+            await app.start()
+        await idle()
         for app in apps:
-            app.stop()
+            await app.stop()
     else:
-        bot.run()
+        await bot.start()
+        await idle()
+        await bot.stop()
 
 if __name__ == "__main__":
-    Thread(target=run_flask).start()
-    Thread(target=run_bot).start()
-
-
+    asyncio.run(main())
 
 
 # Jishu Developer 
