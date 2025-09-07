8c84fb4da2202edc244e14a2fced718c289ff1f0
mafiakp4-cyber <mafiakp4@gmail.com>
2025-09-07 09:59:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,16 @@
-import asyncio
 from pyrogram import Client, idle
 from plugins.cb_data import app as Client2
 from config import *
 import pyromod
 import pyrogram.utils
 from flask import Flask
-import threading
-import os
 
-# Flask app (Render ko port bind karne ke liye)
+# Flask object banaya
 app_web = Flask(__name__)
 
 @app_web.route('/')
 def home():
-    return "Bot is running fine on Render!"
-
-def run_web():
-    port = int(os.environ.get("PORT", 5000))
-    app_web.run(host="0.0.0.0", port=port)
-
-# Start Flask in background
-threading.Thread(target=run_web).start()
+    return "Bot is alive!"
 
 pyrogram.utils.MIN_CHAT_ID = -1002051194704
 pyrogram.utils.MIN_CHANNEL_ID = -1002051194704
@@ -30,25 +20,19 @@ bot = Client(
     bot_token=BOT_TOKEN,
     api_id=API_ID,
     api_hash=API_HASH,
-    plugins=dict(root="plugins")
+    plugins=dict(root='plugins')
 )
 
-async def main():
-    if STRING_SESSION:
-        apps = [Client2, bot]
-        for app in apps:
-            await app.start()
-        await idle()
-        for app in apps:
-            await app.stop()
-    else:
-        await bot.start()
-        await idle()
-        await bot.stop()
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
+if STRING_SESSION:
+    apps = [Client2, bot]
+    for app in apps:
+        app.start()
+    idle()
+    for app in apps:
+        app.stop()
+else:
+    bot.start()
+    idle()
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
